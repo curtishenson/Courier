59e5a83ff690858cdb18a67f29eaffcdde0b1703
curtishenson <svenhenson@gmail.com>
2010-09-11 22:16:22 -0700

    adding redirect

--- courier.rb
+++ courier.rb
@@ -1,12 +1,15 @@
 require 'sinatra'
 require 'pony'
 
-
+error do
+  'Sorry there was an error - ' + env['sinatra.error'].name
+end
 
 post '/' do
   name, email, message = params[:name], params[:email], params[:message]
   
-  Pony.mail(:to => 'svenhenson@gmail.com', 
+  Pony.mail(
+    :to => 'svenhenson@gmail.com', 
     :from => email, 
     :subject => 'Webform submission', 
     :body => message, 
@@ -18,7 +21,9 @@ post '/' do
         :user_name      => ENV['SENDGRID_USERNAME'],
         :password       => ENV['SENDGRID_PASSWORD'],
         :domain         => ENV['SENDGRID_DOMAIN']
-      }
+    }
   )
+  
+  redirect 'http://localhost:4000/contact/'
   
 end
