1dbca9a41e1e0059fe32f02f8351875a2b6fdec9
curtishenson <svenhenson@gmail.com>
2010-09-13 12:09:59 -0700

    adding in success and error redirects

--- courier.rb
+++ courier.rb
@@ -1,29 +1,39 @@
 require 'sinatra'
 require 'pony'
 
+EMAIL_EX = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
+@url = 'http://localhost:4000/contact'
+
 error do
   'Sorry there was an error - ' + env['sinatra.error'].name
 end
 
 post '/' do
+  errors = false;
   name, email, message = params[:name], params[:email], params[:message]
   
-  Pony.mail(
-    :to => 'svenhenson@gmail.com', 
-    :from => email, 
-    :subject => 'Webform submission', 
-    :body => message, 
-    :via => :smtp, 
-    :via_options => {
-        :address        => 'smtp.sendgrid.net',
-        :port           => '25',
-        :authentication => :plain,
-        :user_name      => ENV['SENDGRID_USERNAME'],
-        :password       => ENV['SENDGRID_PASSWORD'],
-        :domain         => ENV['SENDGRID_DOMAIN']
-    }
-  )
+  error = true if email[EMAIL_EX].nil? || name.blank? || message.blank?
   
-  redirect 'http://localhost:4000/contact/'
+  if error == false
+    Pony.mail(
+      :to => 'svenhenson@gmail.com', 
+      :from => email, 
+      :subject => 'Webform submission', 
+      :body => message, 
+      :via => :smtp, 
+      :via_options => {
+          :address        => 'smtp.sendgrid.net',
+          :port           => '25',
+          :authentication => :plain,
+          :user_name      => ENV['SENDGRID_USERNAME'],
+          :password       => ENV['SENDGRID_PASSWORD'],
+          :domain         => ENV['SENDGRID_DOMAIN']
+      }
+    )
+    
+    redirect "#{@url}/success.html"
+  else
+    redirect "#{@url}/contact/error.html"
+  end
   
 end
