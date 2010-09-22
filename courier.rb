85b1e8a87159245c72a507c65f462e6b18249212
curtishenson <svenhenson@gmail.com>
2010-09-21 18:39:04 -0700

    removing error blocks

--- courier.rb
+++ courier.rb
@@ -12,7 +12,7 @@ post '/' do
   errors = false;
   name, email, message = params[:name], params[:email], params[:message]
   
-  error = true if email[EMAIL_EX].nil? || name.blank? || message.blank?
+  
   puts "#{name} -- #{email} -- #{message}"
   if error == false
     Pony.mail(
