b053586226f943588c0e8623a2450fbe0a3735f6
curtishenson <svenhenson@gmail.com>
2010-10-20 14:26:56 -0700

    adding config.yaml file

--- courier.rb
+++ courier.rb
@@ -1,11 +1,20 @@
 require 'sinatra'
 require 'pony'
+require 'yaml'
 
 EMAIL_EX = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
-@@url = 'http://curtishenson.com/contact'
 
 error do
   'Sorry there was an error - ' + env['sinatra.error'].name
+end
+
+configure do
+   config = YAML.load_file("config.yaml")
+   @base_url = config["config"]["base_url"]
+	@email = config["config"]["email"]
+   @email_subject = config["config"]["email_subject"]
+   @success_url = @base_url + "/" + config["config"]["success_url"]
+   @error_url =  @base_url + "/" +config["config"]["error_url"]
 end
 
 post '/' do
@@ -16,9 +25,9 @@ post '/' do
   puts "#{name} -- #{email} -- #{message}"
   if error == false
     Pony.mail(
-      :to => 'svenhenson@gmail.com', 
+      :to => @email, 
       :from => email, 
-      :subject => 'Webform submission', 
+      :subject => @email_subject, 
       :body => message, 
       :via => :smtp, 
       :via_options => {
@@ -31,9 +40,9 @@ post '/' do
       }
     )
     
-    redirect "#{@@url}/success/"
+    redirect @success_url
   else
-    redirect "#{@@url}/error/"
+    redirect @error_url
   end
   
 end
